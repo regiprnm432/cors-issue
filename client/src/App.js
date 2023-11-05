ff661b4c5022d08f35072cfcb4d273eccf35c102
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2023-11-05 18:57:15 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/test') // Ganti URL sesuai dengan URL API Spring Boot Anda
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
--- src/main/java/com/tujuhsembilan/maincore/controller/TestController.java
+++ src/main/java/com/tujuhsembilan/maincore/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.tujuhsembilan.maincore.controller;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@CrossOrigin (origins = "http://localhost:3000")
 @RequestMapping("/test")
 public class TestController {
     
